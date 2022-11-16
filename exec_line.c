e7f31b0d27129a8b4c8aa9df93b85f21f9365632
Adams <106625733+AdamsUthman@users.noreply.github.com>
2022-11-16 10:58:19 +0100

    Edited file

--- exec_line.c
+++ exec_line.c
@@ -4,9 +4,8 @@
  * exec_line - finds builtins and commands
  *
  * @datash: data relevant (args)
- * Return: 1 on success
+ * Return: 1 on success.
  */
-
 int exec_line(data_shell *datash)
 {
 	int (*builtin)(data_shell *datash);
