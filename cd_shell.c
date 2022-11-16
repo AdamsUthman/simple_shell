48d0d0e2321912e8661eaff3da8240b5a3772c4d
Adams <106625733+AdamsUthman@users.noreply.github.com>
2022-11-16 10:49:46 +0100

    Edited file

--- cd_shell.c
+++ cd_shell.c
@@ -6,7 +6,6 @@
  * @datash: data relevant
  * Return: 1 on success
  */
-
 int cd_shell(data_shell *datash)
 {
 	char *dir;
@@ -27,7 +26,13 @@ int cd_shell(data_shell *datash)
 		return (1);
 	}
 
-	if (_strcmp(".", dir) == 0 || _strcmp(".", dir) == 0)
+	if (_strcmp("-", dir) == 0)
+	{
+		cd_previous(datash);
+		return (1);
+	}
+
+	if (_strcmp(".", dir) == 0 || _strcmp("..", dir) == 0)
 	{
 		cd_dot(datash);
 		return (1);
